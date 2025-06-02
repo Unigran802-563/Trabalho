d2ab37835a4091a7aafbf034e9f3d6c02f91ef2f
Unigran802-563 <802.563@alunos.unigran.br>
2025-06-02 11:03:28 -0400

    Correção para o Vercel

--- src/componentes/sidebar/sideBar.jsx
+++ src/componentes/sidebar/sideBar.jsx
@@ -1,4 +1,4 @@
-import './Sidebar.css'
+import './sidebar.css'
 import { useNavigate } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import perfil from '../../assets/perfil.png'
